1d4a25f53e2402ff9071562f2026d4e9106a2a50
imewp <1572819519@qq.com>
2019-06-18 01:00:23 +0800

    主界面

---
+++ SupermarketManage/Main.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace SupermarketManage
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.基本档案ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.员工信息ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.供应商信息ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.进货管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.商品进货ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.商品查询ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.销售管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.商品销售ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.商品退货ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.库存管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.库存查询ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.库存报警ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.系统维护ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.数据备份ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.数据还原ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.辅助工具ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.登录ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.启动WordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.启动ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.计算器ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.直接退出系统ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tsStatusLblUser = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tsStatuslblTime = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.基本档案ToolStripMenuItem,
+            this.进货管理ToolStripMenuItem,
+            this.销售管理ToolStripMenuItem,
+            this.库存管理ToolStripMenuItem,
+            this.系统维护ToolStripMenuItem,
+            this.辅助工具ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1449, 32);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 基本档案ToolStripMenuItem
+            // 
+            this.基本档案ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.员工信息ToolStripMenuItem,
+            this.供应商信息ToolStripMenuItem});
+            this.基本档案ToolStripMenuItem.Name = "基本档案ToolStripMenuItem";
+            this.基本档案ToolStripMenuItem.Size = new System.Drawing.Size(117, 28);
+            this.基本档案ToolStripMenuItem.Text = "基本档案(&B)";
+            // 
+            // 员工信息ToolStripMenuItem
+            // 
+            this.员工信息ToolStripMenuItem.Name = "员工信息ToolStripMenuItem";
+            this.员工信息ToolStripMenuItem.Size = new System.Drawing.Size(210, 30);
+            this.员工信息ToolStripMenuItem.Text = "员工信息";
+            // 
+            // 供应商信息ToolStripMenuItem
+            // 
+            this.供应商信息ToolStripMenuItem.Name = "供应商信息ToolStripMenuItem";
+            this.供应商信息ToolStripMenuItem.Size = new System.Drawing.Size(210, 30);
+            this.供应商信息ToolStripMenuItem.Text = "供应商信息";
+            // 
+            // 进货管理ToolStripMenuItem
+            // 
+            this.进货管理ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.商品进货ToolStripMenuItem,
+            this.商品查询ToolStripMenuItem});
+            this.进货管理ToolStripMenuItem.Name = "进货管理ToolStripMenuItem";
+            this.进货管理ToolStripMenuItem.Size = new System.Drawing.Size(120, 28);
+            this.进货管理ToolStripMenuItem.Text = "进货管理(&H)";
+            // 
+            // 商品进货ToolStripMenuItem
+            // 
+            this.商品进货ToolStripMenuItem.Name = "商品进货ToolStripMenuItem";
+            this.商品进货ToolStripMenuItem.Size = new System.Drawing.Size(210, 30);
+            this.商品进货ToolStripMenuItem.Text = "商品进货管理";
+            // 
+            // 商品查询ToolStripMenuItem
+            // 
+            this.商品查询ToolStripMenuItem.Name = "商品查询ToolStripMenuItem";
+            this.商品查询ToolStripMenuItem.Size = new System.Drawing.Size(210, 30);
+            this.商品查询ToolStripMenuItem.Text = "商品查询";
+            // 
+            // 销售管理ToolStripMenuItem
+            // 
+            this.销售管理ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.商品销售ToolStripMenuItem,
+            this.商品退货ToolStripMenuItem});
+            this.销售管理ToolStripMenuItem.Name = "销售管理ToolStripMenuItem";
+            this.销售管理ToolStripMenuItem.Size = new System.Drawing.Size(118, 28);
+            this.销售管理ToolStripMenuItem.Text = "销售管理(&X)";
+            // 
+            // 商品销售ToolStripMenuItem
+            // 
+            this.商品销售ToolStripMenuItem.Name = "商品销售ToolStripMenuItem";
+            this.商品销售ToolStripMenuItem.Size = new System.Drawing.Size(210, 30);
+            this.商品销售ToolStripMenuItem.Text = "商品销售";
+            // 
+            // 商品退货ToolStripMenuItem
+            // 
+            this.商品退货ToolStripMenuItem.Name = "商品退货ToolStripMenuItem";
+            this.商品退货ToolStripMenuItem.Size = new System.Drawing.Size(210, 30);
+            this.商品退货ToolStripMenuItem.Text = "商品退货";
+            // 
+            // 库存管理ToolStripMenuItem
+            // 
+            this.库存管理ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.库存查询ToolStripMenuItem,
+            this.库存报警ToolStripMenuItem});
+            this.库存管理ToolStripMenuItem.Name = "库存管理ToolStripMenuItem";
+            this.库存管理ToolStripMenuItem.Size = new System.Drawing.Size(117, 28);
+            this.库存管理ToolStripMenuItem.Text = "库存管理(&K)";
+            // 
+            // 库存查询ToolStripMenuItem
+            // 
+            this.库存查询ToolStripMenuItem.Name = "库存查询ToolStripMenuItem";
+            this.库存查询ToolStripMenuItem.Size = new System.Drawing.Size(210, 30);
+            this.库存查询ToolStripMenuItem.Text = "库存查询";
+            // 
+            // 库存报警ToolStripMenuItem
+            // 
+            this.库存报警ToolStripMenuItem.Name = "库存报警ToolStripMenuItem";
+            this.库存报警ToolStripMenuItem.Size = new System.Drawing.Size(210, 30);
+            this.库存报警ToolStripMenuItem.Text = "库存报警";
+            // 
+            // 系统维护ToolStripMenuItem
+            // 
+            this.系统维护ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.数据备份ToolStripMenuItem,
+            this.数据还原ToolStripMenuItem});
+            this.系统维护ToolStripMenuItem.Name = "系统维护ToolStripMenuItem";
+            this.系统维护ToolStripMenuItem.Size = new System.Drawing.Size(116, 28);
+            this.系统维护ToolStripMenuItem.Text = "系统维护(&S)";
+            // 
+            // 数据备份ToolStripMenuItem
+            // 
+            this.数据备份ToolStripMenuItem.Name = "数据备份ToolStripMenuItem";
+            this.数据备份ToolStripMenuItem.Size = new System.Drawing.Size(210, 30);
+            this.数据备份ToolStripMenuItem.Text = "数据备份";
+            // 
+            // 数据还原ToolStripMenuItem
+            // 
+            this.数据还原ToolStripMenuItem.Name = "数据还原ToolStripMenuItem";
+            this.数据还原ToolStripMenuItem.Size = new System.Drawing.Size(210, 30);
+            this.数据还原ToolStripMenuItem.Text = "数据还原";
+            // 
+            // 辅助工具ToolStripMenuItem
+            // 
+            this.辅助工具ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.登录ToolStripMenuItem,
+            this.启动WordToolStripMenuItem,
+            this.启动ToolStripMenuItem,
+            this.计算器ToolStripMenuItem});
+            this.辅助工具ToolStripMenuItem.Name = "辅助工具ToolStripMenuItem";
+            this.辅助工具ToolStripMenuItem.Size = new System.Drawing.Size(116, 28);
+            this.辅助工具ToolStripMenuItem.Text = "辅助工具(&F)";
+            // 
+            // 登录ToolStripMenuItem
+            // 
+            this.登录ToolStripMenuItem.Name = "登录ToolStripMenuItem";
+            this.登录ToolStripMenuItem.Size = new System.Drawing.Size(210, 30);
+            this.登录ToolStripMenuItem.Text = "登录Internet";
+            // 
+            // 启动WordToolStripMenuItem
+            // 
+            this.启动WordToolStripMenuItem.Name = "启动WordToolStripMenuItem";
+            this.启动WordToolStripMenuItem.Size = new System.Drawing.Size(210, 30);
+            this.启动WordToolStripMenuItem.Text = "启动Word";
+            // 
+            // 启动ToolStripMenuItem
+            // 
+            this.启动ToolStripMenuItem.Name = "启动ToolStripMenuItem";
+            this.启动ToolStripMenuItem.Size = new System.Drawing.Size(210, 30);
+            this.启动ToolStripMenuItem.Text = "启动Excel";
+            // 
+            // 计算器ToolStripMenuItem
+            // 
+            this.计算器ToolStripMenuItem.Name = "计算器ToolStripMenuItem";
+            this.计算器ToolStripMenuItem.Size = new System.Drawing.Size(210, 30);
+            this.计算器ToolStripMenuItem.Text = "计算器";
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.直接退出系统ToolStripMenuItem});
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(80, 28);
+            this.退出ToolStripMenuItem.Text = "退出(&T)";
+            // 
+            // 直接退出系统ToolStripMenuItem
+            // 
+            this.直接退出系统ToolStripMenuItem.Name = "直接退出系统ToolStripMenuItem";
+            this.直接退出系统ToolStripMenuItem.Size = new System.Drawing.Size(210, 30);
+            this.直接退出系统ToolStripMenuItem.Text = "直接退出系统";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsStatuslblTime,
+            this.toolStripStatusLabel4,
+            this.tsStatusLblUser});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 817);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1449, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tsStatusLblUser
+            // 
+            this.tsStatusLblUser.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tsStatusLblUser.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenOuter;
+            this.tsStatusLblUser.Name = "tsStatusLblUser";
+            this.tsStatusLblUser.Size = new System.Drawing.Size(462, 23);
+            this.tsStatusLblUser.Spring = true;
+            // 
+            // toolStripStatusLabel4
+            // 
+            this.toolStripStatusLabel4.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.toolStripStatusLabel4.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenOuter;
+            this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+            this.toolStripStatusLabel4.Overflow = System.Windows.Forms.ToolStripItemOverflow.Always;
+            this.toolStripStatusLabel4.Size = new System.Drawing.Size(462, 23);
+            this.toolStripStatusLabel4.Spring = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // tsStatuslblTime
+            // 
+            this.tsStatuslblTime.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tsStatuslblTime.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenOuter;
+            this.tsStatuslblTime.Name = "tsStatuslblTime";
+            this.tsStatuslblTime.Size = new System.Drawing.Size(478, 17);
+            this.tsStatuslblTime.Spring = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1449, 839);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "超市进销存管理系统";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 基本档案ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 员工信息ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 供应商信息ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 进货管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 商品进货ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 商品查询ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 销售管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 商品销售ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 商品退货ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 库存管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 库存查询ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 库存报警ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 系统维护ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 数据备份ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 数据还原ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 辅助工具ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 登录ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 启动WordToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 启动ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 计算器ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 直接退出系统ToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
+        private System.Windows.Forms.ToolStripStatusLabel tsStatusLblUser;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripStatusLabel tsStatuslblTime;
+    }
+}
+
---
+++ SupermarketManage/Main.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SupermarketManage
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+      
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.tsStatuslblTime.Text = "当前时间：" + DateTime.Now.ToString();
+        }
+    }
+}
