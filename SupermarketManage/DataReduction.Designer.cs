92f81d610e7c06ee4e7623a8cebc2f4510ce7ea4
imewp <1572819519@qq.com>
2019-06-20 10:31:29 +0800

    还原

---
+++ SupermarketManage/DataReduction.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace SupermarketManage
+{
+    partial class DataReduction
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DataReduction));
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnReduction = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(50, 150);
+            this.btnOpen.Margin = new System.Windows.Forms.Padding(4);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(87, 34);
+            this.btnOpen.TabIndex = 20;
+            this.btnOpen.Text = "打开(&O)";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Enabled = false;
+            this.txtPath.Location = new System.Drawing.Point(276, 105);
+            this.txtPath.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPath.Multiline = true;
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(324, 107);
+            this.txtPath.TabIndex = 17;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 108);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 18);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "数据备份后的文件路径：";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(99, 29);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(51, 52);
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(24, 29);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(51, 52);
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(171, 29);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(51, 52);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnReduction
+            // 
+            this.btnReduction.Location = new System.Drawing.Point(171, 150);
+            this.btnReduction.Margin = new System.Windows.Forms.Padding(4);
+            this.btnReduction.Name = "btnReduction";
+            this.btnReduction.Size = new System.Drawing.Size(87, 34);
+            this.btnReduction.TabIndex = 21;
+            this.btnReduction.Text = "还原(&H)";
+            this.btnReduction.UseVisualStyleBackColor = true;
+            this.btnReduction.Click += new System.EventHandler(this.btnReduction_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // DataReduction
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 236);
+            this.Controls.Add(this.btnReduction);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "DataReduction";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "数据还原";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnReduction;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
---
+++ SupermarketManage/DataReduction.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SupermarketManage
+{
+    public partial class DataReduction : Form
+    {
+        public DataReduction()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FilterIndex = 0;
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "txt files (*.bak)|*.bak|All files (*.*)|*.*";
+
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtPath.Text = openFileDialog1.FileName.ToString();
+            }
+        }
+
+        private void btnReduction_Click(object sender, EventArgs e)
+        {
+            if (txtPath.Text != "")
+            {
+                getSqlConnection geCon = new getSqlConnection();
+                SqlConnection con = geCon.GetCon();
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+                string DateStr = @"Data Source=WP521\WANGPENG;Initial Catalog=master;Integrated Security=True";
+                SqlConnection conn = new SqlConnection(DateStr);
+                conn.Open();
+
+                //-------------------杀掉所有连接 db_CSManage 数据库的进程--------------
+                string strSQL = "select spid from master..sysprocesses where dbid=db_id( 'SUPERMARKET') ";
+                SqlDataAdapter Da = new SqlDataAdapter(strSQL, conn);
+
+                DataTable spidTable = new DataTable();
+                Da.Fill(spidTable);
+
+                SqlCommand Cmd = new SqlCommand();
+                Cmd.CommandType = CommandType.Text;
+                Cmd.Connection = conn;
+
+                for (int iRow = 0; iRow <= spidTable.Rows.Count - 1; iRow++)
+                {
+                    Cmd.CommandText = "kill " + spidTable.Rows[iRow][0].ToString();   //强行关闭用户进程 
+                    Cmd.ExecuteNonQuery();
+                }
+                conn.Close();
+                conn.Dispose();
+                //--------------------------------------------------------------------
+
+                SqlConnection sqlcon = new SqlConnection(DateStr);
+                sqlcon.Open();
+                SqlCommand sqlCmd = new SqlCommand("backup log SUPERMARKET to disk='" + txtPath.Text.Trim() + "' restore database SUPERMARKET  from disk='" + txtPath.Text.Trim() + "'", sqlcon);
+                sqlCmd.ExecuteNonQuery();
+                sqlCmd.Dispose();
+                sqlcon.Close();
+                sqlcon.Dispose();
+                MessageBox.Show("数据还原成功！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("为了必免数据丢失，在数据库还原后将关闭整个系统。");
+                Application.Exit();
+            }
+            else
+            {
+                MessageBox.Show("请选择备份文件！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+        public bool RestoreDataBase(string databasename,string databasefile)
+       {
+           SqlConnection constring = new SqlConnection("Data Source=WP521\\WANGPENG;Initial Catalog=SUPERMARKET;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False");
+           string sql = "RESTORE DATABASE WITH REPLACE" + databasename + " from DISK ='" + databasefile + " '";//数据库名称和路径 WITH REPLACE是去除日志文件
+           SqlCommand sqlcmd = new SqlCommand(sql, constring);
+           sqlcmd.CommandType = CommandType.Text;
+            
+           try {
+               //开始
+               constring.Open();
+               sqlcmd.ExecuteNonQuery();
+           }catch(Exception ex)
+           {
+               string str = ex.Message;
+               constring.Close();
+               return false;
+           }
+           constring.Close();//结束
+           return true;
+       }
+    }
+}
